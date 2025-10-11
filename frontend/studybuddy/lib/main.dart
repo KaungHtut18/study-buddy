6e4490477a52bd506eab816a7dba84aa69779041
sisshmue <sisshmue404@gmail.com>
2025-10-11 10:25:34 +0700

    add login screen

--- frontend/studybuddy/lib/main.dart
+++ frontend/studybuddy/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:studybuddy/ui/auth/login.dart';
 import 'package:studybuddy/ui/main_screen.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class StudyBuddy extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MainScreen(),
+      home: const Login(),
     );
   }
 }
